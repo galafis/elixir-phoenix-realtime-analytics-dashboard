bdb31b77bb4b9c07eea2b64364f463fcef9349d4
Gabriel Demetrios Lafis <33261275+galafis@users.noreply.github.com>
2026-02-12 09:20:57 -0300

    test: add unit test scaffolding

---
+++ test/main_test.exs
@@ -0,0 +1,12 @@
+defmodule ElixirPhoenixRealtimeAnalyticsDashboardTest do
+  use ExUnit.Case
+
+  test "basic functionality" do
+    assert 2 + 2 == 4
+  end
+
+  test "project files exist" do
+    assert File.exists?("README.md")
+    assert File.exists?("LICENSE")
+  end
+end
